af268177175ff727fc9b486c8557b5db738e62f4
Rubén Darío Carrillo López <rubendario199201@gmail.com>
2024-07-31 00:38:07 -0500

    Install PrimNg

--- web-app/src/app/app.config.ts
+++ web-app/src/app/app.config.ts
@@ -1,9 +1,20 @@
+//Angular
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
+import { routes } from './app.routes';
+//PrimeNG
+import { MessageService } from 'primeng/api';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(),
+    //PrimeNG
+    MessageService,
+    provideAnimations(),
+  ],
 };
