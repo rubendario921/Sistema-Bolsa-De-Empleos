26affce74bf2059e59931043f70b82946b5bf1f9
Rubén Darío Carrillo López <rubendario199201@gmail.com>
2024-08-07 22:57:49 -0500

    compoennt new user
    formulario de registro para nuevo usuario

--- web-app/src/app/main/users/new-user/new-user.component.ts
+++ web-app/src/app/main/users/new-user/new-user.component.ts
@@ -1,6 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,25 +14,50 @@ import { Router } from '@angular/router';
   standalone: true,
   imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './new-user.component.html',
-  styleUrl: './new-user.component.css'
+  styleUrl: './new-user.component.css',
 })
 export class NewUserComponent implements OnInit, OnDestroy {
   //Variables de Componente
-  registerUser!: FormGroup ;  
+  registerUser!: FormGroup;
   // constructor( private fb:FormBuilder, private router: Router){}
-  
+
   ngOnInit(): void {
     //Formulario de registro
     this.registerUser = new FormGroup({
-      name: new FormControl('',[Validators.required, Validators.pattern(/^[a-zA-Z ]+$/),]),
-      lastname: new FormControl('',[Validators.required, Validators.pattern(/^[a-zA-Z ]+$/),]),
-
-
-    })
+      name: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^[a-zA-Z ]+$/),
+      ]),
+      lastName: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^[a-zA-ZÑ ]+$/),
+      ]),
+      typeDni: new FormControl('', [Validators.required]),
+      numDni: new FormControl('', [
+        Validators.required,
+        Validators.minLength(10),
+        Validators.maxLength(10),
+        Validators.pattern(/^[0-9]+$/),
+      ]),
+      phoneNumber: new FormControl('', [
+        Validators.required,
+        Validators.minLength(10),
+        Validators.maxLength(10),
+        Validators.pattern(/^[0-9]+$/),
+      ]),
+      email: new FormControl('', [
+        Validators.required,
+        Validators.email,
+        Validators.pattern(/^[a-z0-9@_.]+$/),
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+      ]),
+    });
   }
-  ngOnDestroy(): void {   
-    this.registerUser.reset();   
+  ngOnDestroy(): void {
+    this.registerUser.reset();
   }
-  onsubmit(){}
-
+  onsubmit() {}
 }
