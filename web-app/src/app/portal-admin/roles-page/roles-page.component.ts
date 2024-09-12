466cd6bcf4754e499191246186817879d15dd3aa
Rubén Darío Carrillo López <rubendario199201@gmail.com>
2024-09-12 00:20:41 -0500

    view-roles-admin

--- web-app/src/app/portal-admin/roles-page/roles-page.component.ts
+++ web-app/src/app/portal-admin/roles-page/roles-page.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { HeaderAdminComponent } from '../template-admin/header-admin/header-admin.component';
 import { FooterAdminComponent } from '../template-admin/footer-admin/footer-admin.component';
 import { NavbarAdminComponent } from '../template-admin/navbar-admin/navbar-admin.component';
+import { ViewRolesAdminComponent } from './view-roles-admin/view-roles-admin.component';
 
 @Component({
   selector: 'app-roles-page',
   standalone: true,
-  imports: [HeaderAdminComponent, FooterAdminComponent, NavbarAdminComponent],
+  imports: [
+    HeaderAdminComponent,
+    FooterAdminComponent,
+    NavbarAdminComponent,
+    ViewRolesAdminComponent,
+  ],
   templateUrl: './roles-page.component.html',
   styleUrl: './roles-page.component.css',
 })
---
+++ web-app/src/app/portal-admin/roles-page/view-roles-admin/view-roles-admin.component.ts
@@ -0,0 +1,75 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { rolesDTO } from '../../../models/rolesDTO';
+import { CustomToastrService } from '../../../services/custom-toastr.service';
+import { RolesService } from '../../../services/roles.service';
+import DataTable from 'datatables.net-dt';
+import { catchError } from 'rxjs';
+
+@Component({
+  selector: 'app-view-roles-admin',
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule, FormsModule],
+  templateUrl: './view-roles-admin.component.html',
+  styleUrl: './view-roles-admin.component.css',
+})
+export class ViewRolesAdminComponent implements OnInit, OnDestroy {
+  //Variables
+  dataTable: any;
+  apiData: any;
+  rolesDetails: rolesDTO[] = [];
+  idRol!: number;
+  //constructor
+  constructor(
+    private toast: CustomToastrService,
+    private rolService: RolesService
+  ) {}
+
+  ngOnInit(): void {
+    this.rolService.getAllRoles().subscribe(
+      (result) => {
+        if (result != null) {
+          this.toast.success('Carga de datos exitosa');
+          this.apiData = result;
+          this.rolesDetails = result;
+          this.loadDataTable();
+        } else {
+          console.error(result);
+          this.toast.error('Error en cargar los datos');
+        }
+      },
+      (error) => {
+        console.error(error);
+        this.toast.error('Error en cargar los datos');
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    if (this.dataTable) {
+      this.dataTable.destroy();
+    }
+  }
+  //Funciones
+  loadDataTable(): void {
+    this.dataTable = new DataTable('#TableRoles', {
+      data: this.apiData,
+      language: {
+        url: 'https://cdn.datatables.net/plug-ins/1.11.5/i18n/es-ES.json',
+      },
+      columns: [
+        { title: '# Id', data: 'rolId' },
+        { title: 'Nombre', data: 'rolName' },
+        { title: 'Descripción', data: 'rolDescription' },
+        {
+          title: 'Acción',
+          data: 'rolId',
+          render(data: any, type: any, full: any) {
+            return `<a href="/usuarios-page/details-users/${full.rolId}" type="button" class="btn btn-warning" title="Ver Detalles"><i class="bi bi-search"></i></a>`;
+          },
+        },
+      ],
+    });
+  }
+}
