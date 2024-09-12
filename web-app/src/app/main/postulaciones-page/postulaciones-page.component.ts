a75356f3a6be6a457a48bbb6e04045a97fcc7524
johnstev01 <140129647+johnstev01@users.noreply.github.com>
2024-09-12 16:13:07 -0500

    Prueba
    solo es una prueba para ver si se muestra lo que hcie

--- web-app/src/app/main/postulaciones-page/postulaciones-page.component.ts
+++ web-app/src/app/main/postulaciones-page/postulaciones-page.component.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
+import { HeaderMainComponent } from "../templates/header-main/header-main.component";
+
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-postulaciones-page',
   standalone: true,
-  imports: [],
+  imports: [HeaderMainComponent, CommonModule],
   templateUrl: './postulaciones-page.component.html',
   styleUrl: './postulaciones-page.component.css'
 })
 export class PostulacionesPageComponent {
+
+// Ejemplo quemado 
+jobList = [
+  {
+    title: 'SUPERVISOR DE OPERACIONES',
+    company: 'CORIS ECUADOR',
+    location: 'Quito, Pichincha',
+    jobType: 'Presencial',
+    published: 'Publicado hoy'
+  },
+  {
+    title: 'Ejecutivo de Ventas Quito',
+    company: 'Norphone',
+    location: 'Quito, Pichincha',
+    jobType: 'Presencial',
+    published: 'Publicado hoy'
+  },
+  {
+    title: 'Ayudante de Cátedra Biología / Anatomía',
+    company: 'QualityUp Soluciones Educativas',
+    location: 'Loja, Loja',
+    jobType: 'Presencial',
+    published: 'Publicado hoy'
+  }
+  // Puedes agregar más empleos aquí
+];
+
 
 }
